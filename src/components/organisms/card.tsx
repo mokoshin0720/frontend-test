b42f105c9260ed2cb8faee9a45d077563c1e47ec
mokoshin0720 <shinya.work0720@gmail.com>
2021-11-08 15:53:06 +0900

    [add]card module scss

--- src/components/organisms/card.tsx
+++ src/components/organisms/card.tsx
@@ -19,15 +19,6 @@ const Card = (props: cardProps) => {
     const [width, height]: number[] = resize(props.width, props.height, 500);
 
     return (
-        // <div className='overflow-hidden rounded-lg h-90 w-80 m-auto bg-white'>
-        //     <div className='max-h-40 w-full object-cover'>
-        //         <Image src={props.img} alt='card' width={width} height={height} />
-        //     </div>
-        //     <div className='w-full p-4'>
-        //         <p className='text-xl font-bold'>{props.title}</p>
-        //         <p className='font-light text-md'>{props.description}</p>
-        //     </div>
-        // </div>
         <div className={style.card}>
             <div className='img'>
                 <Image src={props.img} alt='card' width={width} height={height} />
