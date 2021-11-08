5a428fc7887a8d91596d41e63dff92ffe9ac664f
mokoshin0720 <shinya.work0720@gmail.com>
2021-11-08 15:59:47 +0900

    [move]scss into modules folder

--- src/components/organisms/card.tsx
+++ src/components/organisms/card.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import style from '../../styles/Card.module.scss';
+import style from '../../styles/modules/Card.module.scss';
 
 interface cardProps {
     title: string;
