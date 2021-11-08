26cbac65d2b27abccc8d5c3f85d9c9976a4257d9
mokoshin0720 <shinya.work0720@gmail.com>
2021-11-08 14:35:16 +0900

    [add]resize image

--- src/components/organisms/card.tsx
+++ src/components/organisms/card.tsx
@@ -9,10 +9,13 @@ interface cardProps {
 }
 
 const Card = (props: cardProps) => {
+    const width: number = 500;
+    const height: number = width * (props.height / props.width);
+
     return (
         <div className='overflow-hidden rounded-lg h-90 w-80 m-auto bg-white'>
             <div className='max-h-40 w-full object-cover'>
-                <Image src={props.img} alt='card' width={props.width} height={props.height} />
+                <Image src={props.img} alt='card' width={width} height={height} />
             </div>
             <div className='w-full p-4'>
                 <p className='text-xl font-bold'>{props.title}</p>
