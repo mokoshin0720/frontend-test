2ce006ea933d2331b33adcdb8bdea018ca6c5d1f
mokoshin0720 <shinya.work0720@gmail.com>
2021-11-08 16:58:09 +0900

    [add]index scss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage, GetStaticProps } from 'next';
+import style from '../styles/Index.module.scss';
 import Card from '@/components/organisms/card';
 import getPhotos, { Photos } from 'lib/photos';
 
@@ -15,28 +16,48 @@ export const getStaticProps: GetStaticProps | undefined = async () => {
 
 const Home: NextPage<Photos> = ({ contents }) => {
     return (
-        <div className='bg-gray-100'>
-            <div className='pl-10 pt-8'>
-                <h1 className='text-2xl font-bold'>投稿一覧</h1>
+        <div className={style.container}>
+            <div className='header'>
+                <h1 className='page-title'>投稿一覧</h1>
             </div>
-            <div>
-                <div>
-                    {contents.map((content, key) => {
-                        return (
-                            <div className='p-7' key={key}>
-                                <Card
-                                    title={content.title}
-                                    description={content.description}
-                                    img={content.image.url}
-                                    width={content.image.width}
-                                    height={content.image.height}
-                                />
-                            </div>
-                        );
-                    })}
-                </div>
+            <div className={style.contents}>
+                {contents.map((content, key) => {
+                    return (
+                        <div key={key} className='card'>
+                            <Card
+                                title={content.title}
+                                description={content.description}
+                                img={content.image.url}
+                                width={content.image.width}
+                                height={content.image.height}
+                            />
+                        </div>
+                    );
+                })}
             </div>
         </div>
+        // <div className='bg-gray-100'>
+        //     <div className='pl-10 pt-8'>
+        //         <h1 className='text-2xl font-bold'>投稿一覧</h1>
+        //     </div>
+        //     <div>
+        //         <div>
+        //             {contents.map((content, key) => {
+        //                 return (
+        //                     <div className='p-7' key={key}>
+        //                         <Card
+        //                             title={content.title}
+        //                             description={content.description}
+        //                             img={content.image.url}
+        //                             width={content.image.width}
+        //                             height={content.image.height}
+        //                         />
+        //                     </div>
+        //                 );
+        //             })}
+        //         </div>
+        //     </div>
+        // </div>
     );
 };
 
