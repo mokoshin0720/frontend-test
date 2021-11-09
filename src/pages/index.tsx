3c3c1a581c291dae61597012552170f9e0ffe61e
mokoshin0720 <shinya.work0720@gmail.com>
2021-11-09 16:12:53 +0900

    [add]contents css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,28 +36,6 @@ const Home: NextPage<Photos> = ({ contents }) => {
                 })}
             </div>
         </div>
-        // <div className='bg-gray-100'>
-        //     <div className='pl-10 pt-8'>
-        //         <h1 className='text-2xl font-bold'>投稿一覧</h1>
-        //     </div>
-        //     <div>
-        //         <div>
-        //             {contents.map((content, key) => {
-        //                 return (
-        //                     <div className='p-7' key={key}>
-        //                         <Card
-        //                             title={content.title}
-        //                             description={content.description}
-        //                             img={content.image.url}
-        //                             width={content.image.width}
-        //                             height={content.image.height}
-        //                         />
-        //                     </div>
-        //                 );
-        //             })}
-        //         </div>
-        //     </div>
-        // </div>
     );
 };
 
